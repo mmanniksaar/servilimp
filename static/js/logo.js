72e49d9d4b41194492b40fd1387c3f7f3f0d5393
Marek Männiksaar <m.manniksaar@gmail.com>
2024-01-17 19:16:43 +0200

    logo move endpoint set

--- static/js/logo.js
+++ static/js/logo.js
@@ -1,45 +1,9 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-    const logo = document.getElementById("animated-logo");
-
-    gsap.to(logo, {
-        duration: 2,
-        x: "-500%",
-        rotation: 360, // Lisa pöörlemine liikumise animatsiooni sisse
-        ease: "power1.inOut",
-        onComplete: function () {
-            // Funktsioon, mis käivitatakse liikumise ja pöörlemise lõppedes
-            console.log("Liikumine ja pöörlemine on lõppenud");
-        }
-    });
-
-    // Pöörlemise funktsioon
-    function rotateLogo() {
-        gsap.to(logo, { rotation: "+=360", ease: "power1.inOut" });
-    }
-
-    // Pöörlemise peatamise funktsioon
-    function stopRotation() {
-        gsap.to(logo, { rotation: 0, ease: "power1.inOut" });
-    }
-
-    // Logo pöörlemise sündmustiku kuulajad
-    logo.addEventListener("mouseover", rotateLogo);
-    logo.addEventListener("mouseout", stopRotation);
-
-    gsap.to(".choice-container", { 
-        duration: 3, // Animatsiooni kestus sekundites
-        y: "-50%",   // Y-telg (vertikaalne) muutus
-        ease: "power2.out" // Animatsiooni lihtsustamise funktsioon
-    
-    });
-});
-*/
-
 document.addEventListener("DOMContentLoaded", function () {
     const logo = document.getElementById("animated-logo");
 
     // Funktsioon, mis arvutab animatsiooni alguspunkti sõltuvalt ekraani laiusest
     function calculateStartPosition() {
+        screenWidth = 0;
         const screenWidth = window.innerWidth;
         const logoWidth = logo.offsetWidth;
         const startPosition = screenWidth + (logoWidth + 5 / 100 * screenWidth); // Muutus siin
@@ -48,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     gsap.to(logo, {
         duration: 2,
-        x: -calculateStartPosition(), // Muutus siin
+        x: -calculateStartPosition(),
         rotation: 360,
         ease: "power1.inOut",
         onComplete: function () {
