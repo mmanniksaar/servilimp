3445a4a2b0ae7d26910a7709df4b1bca5277db16
Marek Männiksaar <m.manniksaar@gmail.com>
2024-01-29 21:53:53 +0200

    page Contact Us img added

--- static/js/clients_zoom.js
+++ static/js/clients_zoom.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
   var modal = document.getElementById("myModal");
   var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
+  /* var captionText = document.getElementById("caption"); */
 
   $('.client-container img').click(function() {
       modal.style.display = "block";
       modalImg.src = $(this).attr('src');
-      captionText.innerHTML = $(this).attr('alt');
+      /* captionText.innerHTML = $(this).attr('alt'); */
 
-      // Lisage alljärgnev kood, et muuta modalImg.src vastavalt client_image URL-ile
       var clientImageSrc = $(this).data('client-image-src');
       modalImg.src = clientImageSrc;
   });
 
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == modal || event.target == modalImg) {
       modal.style.display = "none";
