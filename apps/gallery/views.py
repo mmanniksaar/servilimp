d50a4fbab666945f550837de1896fe35a9e11c2d
Marek Männiksaar <m.manniksaar@gmail.com>
2024-02-01 17:05:48 +0200

    metadata

--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -2,19 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Gallery
 
 
-""" def gallery_view(request):
+def gallery_view(request):
     gallery_items = Gallery.objects.all()
     
     context = {
         'gallery_items': gallery_items,
     }
-    return render(request, 'gallery.html' ,context) """
+    return render(request, 'gallery.html' ,context)
 
 
-def gallery_view(request, id):
-    template = 'gallery.html'
-    gallery_items = Gallery.objects.get(pk=id)
-    context = {}
-    context['gallery_items'] = gallery_items
-    context['meta'] = gallery_items.as_meta()
-    return render(request, template, context)
--- static/js/gallery_zoom.js
+++ static/js/gallery_zoom.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
