4c3965d3915dae2ec009e6a8fc870a41b04946d3
Marek Männiksaar <m.manniksaar@gmail.com>
2024-11-06 11:26:55 +0200

    whitenose

--- project_Servilimp/settings.py
+++ project_Servilimp/settings.py
@@ -16,6 +16,7 @@ ALLOWED_HOSTS = ['servilimp.fi', 'www.servilimp.fi', '127.0.0.1', 'servilimp-16a
 
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
